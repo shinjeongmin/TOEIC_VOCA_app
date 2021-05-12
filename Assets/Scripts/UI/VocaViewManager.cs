2f14d7eb775cd2c70cd47a66a2df8e7efaa374f9
chocoCO3 <71392348+SKT-T1-BUSH@users.noreply.github.com>
2021-05-12 19:21:33 +0900

    Update VocaViewManager.cs

--- Assets/Scripts/UI/VocaViewManager.cs
+++ Assets/Scripts/UI/VocaViewManager.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VocaViewManager : MonoBehaviour
+public class VocaViewManager : MonoBehaviour //VocaViewManager 정의, 이것도 모노로 되어있음 
 {
     private int curVocaIdx = 0;
-    public GameObject ViewPanel_voca;
-    public GameObject ViewPanel_mean;
-    public GameObject PrevBtn;
-    public GameObject NextBtn;
-    public GameObject ViewPanel_day;
+    public GameObject ViewPanel_voca;//유니티 상에서 Scene에 존재하는 오브젝트(약간 버튼 같은건가?)를 선언, 이름이 ViewPanel_voca
+    public GameObject ViewPanel_mean;//오브젝트 선언, 이름이 ViewPanel_mean
+    public GameObject PrevBtn;//오브젝트 선언, 이름이 PrevBtn
+    public GameObject NextBtn;//오브젝트 선언, 이름이 NextBtn
+    public GameObject ViewPanel_day;//오브젝트 선언, 이름이 ViewPanel_day
 
     public void Initialize()
     {
@@ -28,7 +28,7 @@ public class VocaViewManager : MonoBehaviour
         // 단어 및 뜻 설정
         ViewPanel_voca.GetComponent<Text>().text
             = VocabularyDataBase.GetVocabularyLists()[curVocaIdx].vocabulary;
-        ViewPanel_mean.GetComponent<Text>().text 
+        ViewPanel_mean.GetComponent<Text>().text
             = VocabularyDataBase.GetVocabularyLists()[curVocaIdx].meaning;
         // 단어 일차 설정
         ViewPanel_day.GetComponent<Text>().text
@@ -40,7 +40,7 @@ public class VocaViewManager : MonoBehaviour
     /// </summary>
     private void InitVocaDay()
     {
-        for(int i=1; i < VocabularyDataBase.GetVocabularyLists().Count; i++)
+        for (int i = 1; i < VocabularyDataBase.GetVocabularyLists().Count; i++)
         {
             // 엑셀의 빈칸의 경우 비어있지는 않은 공백으로 판단됨
             if (string.IsNullOrWhiteSpace(VocabularyDataBase.GetVocabularyDay(i)))
@@ -57,7 +57,7 @@ public class VocaViewManager : MonoBehaviour
             curVocaIdx++;
             SetVoca2ViewPanel();
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             curVocaIdx = VocabularyDataBase.GetVocabularyLists().Count - 1;
             Debug.LogWarning("Last VOCA" + curVocaIdx);
